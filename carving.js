95f3bb389858d5867a2ed27debba5f172d40c262
Youngseok Bae <young.seok@hotmail.com>
2021-04-08 16:42:37 +0000

    commiting Area Calculator exercise

---
+++ carving.js
@@ -0,0 +1,47 @@
+const calculateRectangleArea = function (length, width) {
+  if (length < 0 || width < 0) {
+    return undefined;
+  }
+  else {
+    let recArea = length * width;
+    return recArea;
+  }
+  
+
+}
+
+const calculateTriangleArea = function (base, height){
+  if (base < 0 || height < 0) {
+    return undefined;
+  }
+  else {
+    let triArea = base * height / 2;
+    return triArea;
+  }
+  
+
+}
+
+const calculateCircleArea = function (radius) {
+  if (radius < 0) {
+    return undefined;
+  }
+  else {
+    let cirArea = Math.PI * (radius * radius);
+    return cirArea;
+  }
+ 
+
+}
+
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
