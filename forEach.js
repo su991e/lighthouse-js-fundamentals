27975d8f7a31965d115a20cb86ac7198d2e18044
Youngseok Bae <young.seok@hotmail.com>
2021-04-09 15:36:14 +0000

    forEach loop exercise

---
+++ forEach.js
@@ -0,0 +1,16 @@
+var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
+  19, 300, 3775, 299, 36, 209, 148, 169, 299,
+  6, 109, 20, 58, 139, 59, 3, 1, 139
+];
+
+// Write your code here
+
+test.forEach(function(num,ind) {
+  if (num%3 === 0) {
+      test[ind] += 100;
+  }
+  
+
+});
+
+console.log(test)
