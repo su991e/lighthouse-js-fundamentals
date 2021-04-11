6b3821661bbeed54e6d6f60fa68f2032ba024045
Youngseok Bae <young.seok@hotmail.com>
2021-04-11 02:55:42 +0000

    Looping-lastIndexOf exercise

---
+++ looping-lastindex.js
@@ -0,0 +1,19 @@
+let nums = [];
+
+//define index of last value
+let lastIndexOf = function (nums, numb) {
+
+  for (let a = nums.length; a >= 0; a--) {
+    if (numb === nums[a]) {
+      return a;
+    }
+
+  }
+  return -1;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
